fa11d454bd44efff7154e04ac5c2095ed2bb9b6e
Davi Amaral <131984949+DaviLAmaral@users.noreply.github.com>
2023-10-10 11:24:10 -0300

    Add files via upload

---
+++ Pokedex/Setup.py
@@ -0,0 +1,23 @@
+import subprocess
+libraries_to_install = [
+    'requests',
+    'beautifulsoup4',
+    'pandas',
+    'Pillow',
+    'tk',
+    're',
+    'numpy',
+    'matplotlib',
+    'openpyxl'
+]
+
+def install_libraries():
+    for library in libraries_to_install:
+        try:
+            subprocess.check_call(['pip', 'install', library])
+            print(f'Successfully installed {library}')
+        except subprocess.CalledProcessError:
+            print(f'Failed to install {library}')
+
+if __name__ == "__main__":
+    install_libraries()
